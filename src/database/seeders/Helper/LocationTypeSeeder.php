06d4290bdda46229e8c2a971181e697722ff1436
Nayem Uddin <nayemuddincucse@gmail.com>
2025-03-19 15:11:50 +0600

    Added type column

--- src/database/seeders/Helper/LocationTypeSeeder.php
+++ src/database/seeders/Helper/LocationTypeSeeder.php
@@ -15,30 +15,44 @@ class LocationTypeSeeder extends Seeder
             [
                 'name' => 'In-person meeting',
                 'description' => 'Set a physical address',
+                'type' => 'physical'
             ],
             [
                 'name' => 'Phone call',
                 'description' => 'Inbound or outbound calls',
+                'type' => 'phone'
+
+
             ],
             [
                 'name' => 'Telehealth video call',
                 'description' => 'Set a virtual location',
+                'type' => 'remote'
+
             ],
             [
                 'name' => 'Google Meet',
                 'description' => 'Web conference',
+                'type' => 'remote'
+
             ],
             [
                 'name' => 'Zoom',
                 'description' => 'Web conference / virtual location',
+                'type' => 'remote'
+
             ],
             [
                 'name' => 'Doxy.me',
                 'description' => 'Web conference / virtual location',
+                'type' => 'remote'
+
             ],
             [
                 'name' => 'Microsoft Teams',
                 'description' => 'Web conference / virtual location',
+                'type' => 'remote'
+
             ],
         ];
 
