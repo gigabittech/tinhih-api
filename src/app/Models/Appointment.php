1cf6d5f5a131905502c301a889b6f004d7ef4326
Nayem Uddin <nayemuddincucse@gmail.com>
2025-03-20 15:54:16 +0600

    git status

--- src/app/Models/Appointment.php
+++ src/app/Models/Appointment.php
@@ -9,6 +9,22 @@ class Appointment extends Model
     protected $fillable = [
         'date',
         'time',
-
+        'description'
     ];
+
+
+    public function locations()
+    {
+        return $this->belongsTo(Location::class, 'appointment_location');
+    }
+
+    public function services()
+    {
+        return $this->belongsTo(Service::class, 'appointment_service');
+    }
+
+    public function attendees()
+    {
+        return $this->belongsTo(User::class, 'appointment_attendee');
+    }
 }
