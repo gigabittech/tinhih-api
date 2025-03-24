2699c5deeaa5bec3eb54efd1093b68855b29402a
Nayem Uddin <nayemuddincucse@gmail.com>
2025-03-24 15:35:53 +0600

    new routes are implemented

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\v1\AppointmentController;
 use App\Http\Controllers\v1\AuthController;
+use App\Http\Controllers\v1\ClientController;
 use App\Http\Controllers\v1\LocationController;
 use App\Http\Controllers\v1\LocationTypeController;
 use App\Http\Controllers\v1\ServiceController;
+use App\Http\Controllers\v1\TeamMemberController;
+use App\Http\Controllers\v1\WorkspaceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -92,6 +96,7 @@ Route::prefix('v1')->group(function () {
             Route::put('/{id}', [LocationController::class, 'updateLocation']);
             Route::delete('/{id}', [LocationController::class, 'deleteLocation']);
         });
+
         Route::prefix('services')->group(function () {
             Route::get('/', [ServiceController::class, 'getservices']);
             Route::get('/user', [ServiceController::class, 'getServicesByUser']);
@@ -99,6 +104,40 @@ Route::prefix('v1')->group(function () {
             Route::post('/', [ServiceController::class, 'createservice']);
             Route::put('/{id}', [ServiceController::class, 'updateservice']);
             Route::delete('/{id}', [ServiceController::class, 'deleteservice']);
+        });
+
+        Route::prefix('members')->group(function () {
+            Route::get('/', [TeamMemberController::class, 'getMembers']);
+            Route::get('/{id}', [TeamMemberController::class, 'getMember']);
+            Route::post('/', [TeamMemberController::class, 'createMember']);
+            Route::put('/{id}', [TeamMemberController::class, 'updateMember']);
+            Route::delete('/{id}', [TeamMemberController::class, 'deleteMember']);
+        });
+
+        Route::prefix('workspaces')->group(function () {
+            Route::get('/', [WorkspaceController::class, 'getWorkspaces']);
+            Route::get('/user', [WorkspaceController::class, 'getUserWorkspaces']);
+            Route::get('/{id}', [WorkspaceController::class, 'getWorkspace']);
+            Route::post('/', [WorkspaceController::class, 'createWorkspace']);
+            Route::post('/toggle', [WorkspaceController::class, 'toggleWorkspace']);
+            Route::put('/{id}', [WorkspaceController::class, 'updateWorkspace']);
+            Route::delete('/{id}', [WorkspaceController::class, 'deleteWorkspace']);
+        });
+
+        Route::prefix('appointments')->group(function () {
+            Route::get('/', [AppointmentController::class, 'getAppointments']);
+            Route::get('/{id}', [AppointmentController::class, 'getAppointment']);
+            Route::post('/', [AppointmentController::class, 'createAppointment']);
+            Route::put('/{id}', [AppointmentController::class, 'updateAppointment']);
+            Route::delete('/{id}', [AppointmentController::class, 'deleteAppointment']);
+        });
+
+        Route::prefix('clients')->group(function () {
+            Route::get('/', [ClientController::class, 'getClients']);
+            Route::get('/{id}', [ClientController::class, 'getClient']);
+            Route::post('/', [ClientController::class, 'createClient']);
+            Route::put('/{id}', [ClientController::class, 'updateClient']);
+            Route::delete('/{id}', [ClientController::class, 'deleteClient']);
         });
     });
 });
