67cfd8a8e5b335e4c9ab486b4f6a9fb1fbe7af0e
Nayem Uddin <nayemuddincucse@gmail.com>
2025-04-06 15:36:34 +0600

    structured tax module in invoice module

---
+++ src/database/migrations/2025_04_06_084032_create_taxes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('workspace_id')->constrained('workspaces')->onDelete('cascade');
+            $table->string('name');
+            $table->integer('percentage')->default(0);
+            $table->boolean('is_default')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('taxes');
+    }
+};
