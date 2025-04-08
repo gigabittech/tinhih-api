162e1825a97caf527ed5553880cbdc492c1d0793
Nayem Uddin <nayemuddincucse@gmail.com>
2025-04-08 16:37:21 +0600

    registered middleware

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -14,12 +14,14 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
+        apiPrefix: '',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => RoleMiddleware::class,
+            'verifyWorkspaceSetup' => \App\Http\Middleware\WorkspaceSetupMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
